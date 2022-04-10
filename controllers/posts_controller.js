0227588e102e51d3ac57aa02054d9c125124c068
Vivek <vivekraj78965.vr@gmail.com>
2022-04-10 15:22:50 +0530

    added delete action

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,8 +4,9 @@
 //     });
 // }
 
-
 const Post  = require('../models/post');
+const Comment = require('../models/comment')
+
 module.exports.create = function(req , res){
      Post.create({
          content:req.body.content,
@@ -18,4 +19,18 @@ module.exports.create = function(req , res){
      });
 }
 
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        // .id means converting the object id into string
+        if (post.user == req.user.id){
+            post.remove();
 
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+
+    });
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,12 @@
  const myExpress = require('express');
  const router = myExpress.Router();
+ const passport = require('passport');
 
  const postController = require('../controllers/posts_controller');
 
- router.post('/create', postController.create);
+ router.post('/create', passport.checkAuthentication , postController.create);
+
+ router.get('/destroy/:id', passport.checkAuthentication , postController.destroy)
 
  module.exports = router;
 
