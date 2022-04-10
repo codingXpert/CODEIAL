b50d03a1fcaedd935337deb8e8e371835d79e5e1
Vivek <vivekraj78965.vr@gmail.com>
2022-04-10 18:37:03 +0530

    User profile links AND Updating a User's Profile

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -7,29 +7,26 @@ module.exports.myHome = function (req, res) {
 
 
 
-// populate the user of each post
-Post.find({})
-.populate('user')
-.populate({
-    path:'comments',
-    populate:{
-        path:'user'
-    }
-})
-.exec(function(err, posts){
+    // populate the user of each post
+    Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function (err, posts) {
 
-User.find({} , function(err , users){
-
-    return res.render('home', {
-        title: "Codeial | Home",
-        posts:  posts,
-        all_users:users
-    });
-});
-   
-});
+            User.find({}, function (err, users) {
+                return res.render('home', {
+                    title: "Codeial | Home",
+                    posts: posts,
+                    all_users: users
+                });
+            });
 
 
+        })
 
-
- }  
+}
