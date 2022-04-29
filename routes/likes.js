879eab8c22816c13aef5674d3f125a6b1503e9a4
Vivek <vivekraj78965.vr@gmail.com>
2022-04-29 17:39:08 +0530

    Actions and Routes :: Likes

---
+++ routes/likes.js
@@ -0,0 +1,9 @@
+const myExpress = require('express');
+const router = myExpress.Router();
+const likesController = require('../controllers/likes_controller');
+
+
+router.post('/toggle , likesController.toggleLike');
+
+
+module.exports = router;
